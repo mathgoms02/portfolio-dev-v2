9f681355f1d2629a6368b4fe63f2e7a9ce6774ec
mathgoms02 <math.gomsx3@gmail.com>
2026-05-01 23:58:38 -0300

    chore(front): Updated slide text with fade effect

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ let isDeleting = false;
 // Carrousel Variabels
 const slide = document.querySelector(".carousel-slide");
 const items = document.querySelectorAll(".slide");
+const slideTextBox = document.querySelectorAll(".slide-text");
 const nextBtn = document.getElementById("nextBtn");
 const prevBtn = document.getElementById("prevBtn");
 
@@ -23,6 +24,7 @@ const size = items[0].getBoundingClientRect().width;
 
 // Initial Position
 slide.style.transform = "translateX(0px)";
+slideTextBox[0].classList.add("fade-in");
 
 // Typewriter
 function typewriter() {
@@ -58,11 +60,35 @@ document.addEventListener("DOMContentLoaded", typewriter);
 nextBtn.addEventListener("click", () => {
   if (counter >= items.length - 1) return;
   counter++;
+
+  const currentText = document.querySelector(`.sl${counter + 1}`);
+  const lastText = document.querySelector(`.sl${counter}`);
+
   slide.style.transform = `translateX(${-size * counter}px)`;
+
+  lastText.classList.add("fade-out");
+  currentText.classList.add("fade-in");
+
+  setTimeout(() => {
+    lastText.classList.remove("fade-in");
+    lastText.classList.remove("fade-out");
+  }, 500);
 });
 
 prevBtn.addEventListener("click", () => {
   if (counter <= 0) return;
   counter--;
+
+  const currentText = document.querySelector(`.sl${counter + 1}`);
+  const lastText = document.querySelector(`.sl${counter + 2}`);
+
   slide.style.transform = `translateX(${-size * counter}px)`;
+
+  lastText.classList.add("fade-out");
+  currentText.classList.add("fade-in");
+
+  setTimeout(() => {
+    lastText.classList.remove("fade-in");
+    lastText.classList.remove("fade-out");
+  }, 500);
 });
